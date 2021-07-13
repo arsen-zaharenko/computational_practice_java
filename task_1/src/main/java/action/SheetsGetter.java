26c5ef1779f858a4eac2c4577ace4a088d228741
arsen-zaharenko <59254411+arsen-zaharenko@users.noreply.github.com>
2021-07-13 16:38:45 +0300

    task_1

---
+++ task_1/src/main/java/action/SheetsGetter.java
@@ -0,0 +1,22 @@
+package action;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class SheetsGetter {
+    private static final String APPLICATION_NAME = "Google Sheets and Java";
+
+    public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
+        Credential credential = Authorization.authorize();
+
+        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(), credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+}
---
+++ task_1/src/main/java/run/Main.java
@@ -0,0 +1,35 @@
+package run;
+
+import action.SheetsGetter;
+import com.google.api.services.sheets.v4.Sheets;
+import entity.Student;
+import task.Task;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+public class Main {
+    private static final String SPREADSHEET_ID = "1YcFgajkOvKnS5r9O054rqrnACnqloooMr4iKjDA31Sw";
+    private static final String RANGE  = "A2:F";
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        Sheets sheetsService = SheetsGetter.getSheetsService();
+
+        //write data
+        Task.add(sheetsService, SPREADSHEET_ID, new Student("John","Male","4. Senior","LA","Math","Football"));
+        Task.add(sheetsService, SPREADSHEET_ID, new Student("Michael","Male","1. Freshman","NY","IT","Cricket"));
+        Task.add(sheetsService, SPREADSHEET_ID, new Student("Angela","Female","2. Sophomore","WI","Biology","Swimming"));
+
+        //read data
+        List<Student> students = Task.read(sheetsService, SPREADSHEET_ID, RANGE);
+
+        if (!students.isEmpty()) {
+            for (Student student : students) {
+                System.out.println(student + "\n");
+            }
+        } else {
+            System.out.println("List is empty.");
+        }
+    }
+}
